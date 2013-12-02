298581433fc512cd044624cf5bf8d18793be1979
Leo Silva <leo.moreira@me.com>
2013-12-02 12:32:11 -0300

    alterações gerais...

--- Main.groovy
+++ Main.groovy
@@ -8,8 +8,10 @@ class Main {
 			println "Bem-vindo a Urna Eletrônica do IFRN! Digite a sua opção:"
 			println "1 - Cadastro de Candidatos"
 			println "2 - Iniciar Votação"
-			println "3 - Contar resultados"
-			println "4 - Sair"
+			println "3 - Contar resultados (quantidade de votos)"
+			println "4 - Contar resultados (porcentagem)"
+			println "5 - Verificar se haverá segundo turno"
+			println "6 - Sair"
 			def opcao = it.readLine() as Integer
 			verificaOpcao(opcao)
 		}
@@ -25,10 +27,13 @@ class Main {
 			case 2:
 				Votacao.iniciar()
 				break
-			case 3:
-				ContaResultado.calcular()
+			case 3..4:
+				ContaResultado.calcular(opcao)
 				break
-			case 4:
+			case 5:
+				VerificarSegundoTurno.verificar()
+				break
+			case 6:
 				println "Tchau!"
 				System.exit(0)
 			default:
