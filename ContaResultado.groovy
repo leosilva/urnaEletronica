3ba7e8f2667617f7a2e1d787b8e5c7f656a26f14
evangilo <evangilo@hotmail.com>
2013-12-03 17:57:50 -0300

    calcular porcentagem de votos implementada

--- ContaResultado.groovy
+++ ContaResultado.groovy
@@ -1,14 +1,27 @@
 class ContaResultado {
 
-	def static calcular (op) {
+	static def votos;
 
-		calcularVotosQuantidade()
+	def static calcular (op) {
+		if (op == 3)
+			calcularVotosQuantidade()
+		else (op == 4)
+			calcularPorcetagemVotos ()
 	}
 
 	def static calcularVotosQuantidade() {
 		Votacao.mapaVotacao.each { numero, qtdVotos ->
 			def nome = CadastraCandidatos.mapaCandidatos[numero]
 			println "Chapa $numero | Nome $nome | Quantidade de votos $qtdVotos"
+		}
+	}
+
+	def static calcularPorcetagemVotos () {
+		votos = Votacao.mapaVotacao.values().sum()
+		Votacao.mapaVotacao.each { numero, qtdVotos ->
+			def nome = CadastraCandidatos.mapaCandidatos[numero]
+			def porcentagemVotos = qtdVotos / votos * 100
+			println "Chapa $numero | Nome $nome | Porcentagme de votos $porcentagemVotos"
 		}
 	}
 }
