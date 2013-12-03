714cd396db84b2657e93a38e17d848d8fce78310
evangilo <evangilo@hotmail.com>
2013-12-03 17:06:59 -0300

    implementado a contagem

--- ContaResultado.groovy
+++ ContaResultado.groovy
@@ -1,3 +1,19 @@
 class ContaResultado {
+
+	def static votos = [:]
+
+	def static calcular (op) {
+		CadastraCandidatos.mapaCandidatos.each { numero, nome ->
+			println "Nome: $nome  --  Chapa $numero"
+		}
+	}
+
+	def calcularVotosPorcetagem() {
+
+	}
+
+	def calcularVotosQuantidade() {
+		
+	}
 	
 }
