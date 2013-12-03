20658e9500ce2a52ba8b25859e8f6a2377091b94
Jefferson Lima <jefferson.fl123@gmail.com>
2013-12-03 18:11:01 -0300

    Finalizando Segundo Turno

--- ContaResultado.groovy
+++ ContaResultado.groovy
@@ -1,3 +1,14 @@
 class ContaResultado {
-	
+	def static mapaPorcentagem = [:]
+	def contarResultadoEmPorcentagem(){
+		
+		def contagem = 0
+		Votacao.mapaVotacao.each{
+			contagem += mapaVotacao[it]
+		}
+		
+		Votacao.mapaVotacao.each{chave, valor ->
+			mapaPorcentagem[chave] = (valor*100)/contagem
+		}
+	}
 }
--- VerificarSegundoTurno.groovy
+++ VerificarSegundoTurno.groovy
@@ -1,3 +1,23 @@
 class VerificarSegundoTurno {
-	
+	def segundoTurno = true
+	def mapaSegundTurno = [:]
+	def verificarSegundoTurno(){
+		ContaResultado.mapaPorcentagem.each{chave,valor ->
+			if (valor > 50){
+				segundoTurno = false
+			}
+		}
+
+		if (segundoTurno) {
+
+			def mapaChaves = (CadastraCandidatos.mapaCandidatos.sort{
+				it.value
+			}).keySet()
+			mapaSegundTurno[mapaChaves[0]] = CadastraCandidatos.mapaCandidatos[mapaChaves[0]]
+			mapaSegundTurno[mapaChaves[1]] = CadastraCandidatos.mapaCandidatos[mapaChaves[1]]
+			CadastraCandidatos.mapaCandidatos = mapaSegundTurno
+
+			Votacao.iniciar()
+		}
+	}
 }
