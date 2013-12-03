a3faccbedb2b88ef3d326541feeff8658af933aa
= <leo.moreira@me.com>
2013-12-03 16:56:05 -0300

    implementação da tarefa de votação

--- Votacao.groovy
+++ Votacao.groovy
@@ -1,3 +1,36 @@
 class Votacao {
-	
+
+	static def mapaVotacao = [:]
+
+	static def iniciar() {
+		println "Iniciando a votação..."
+		CadastraCandidatos.mapaCandidatos.keySet().each {
+			mapaVotacao[it] = 0
+		}
+		println """
+		Instruções: 
+			- Digite o número do candidato a ser votado;
+			- Para encerrar a votação, digite 0.
+		Votação em andamento..."""
+		realizarVotacao()
+	}
+
+	static def realizarVotacao() {
+		System.in.withReader {
+			println "Digite o número do candidato: "
+			def numeroDigitado = it.readLine() as Integer
+			if (!numeroDigitado) {
+				println "Votação encerrada!"
+				Main.inicio()
+			} else if (!mapaVotacao.keySet().contains(numeroDigitado)) {
+				println "Candidato inexistente!"
+				realizarVotacao()
+			}
+			mapaVotacao[numeroDigitado] = mapaVotacao[numeroDigitado] + 1
+			println mapaVotacao[numeroDigitado]
+			println "Voto computado com sucesso!"
+			realizarVotacao()
+		}
+	}
+		
 }
